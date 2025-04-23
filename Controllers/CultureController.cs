3bd53dd774d3fc6132e1a76aa2c206ae3fe05452
Megi Andreevska <megi@edusoft2.com>
2025-04-23 13:18:35 +0200

    nova verzija

--- Controllers/CultureController.cs
+++ Controllers/CultureController.cs
@@ -6,21 +6,42 @@ using System;
 public class CultureController : Controller
 {
     [HttpGet("setculture")]
-    public IActionResult SetCulture(string culture, string redirectUri)
+    public IActionResult SetCulture(string culture = "en-US", string redirectUri = "/")
     {
+        Console.WriteLine($"➡️ Се повикува CultureController.SetCulture со culture: {culture}");
+
         if (!string.IsNullOrEmpty(culture))
         {
-            // Поставување на културата во колачињата
+            // Поставување на кука за култура
             Response.Cookies.Append(
                 CookieRequestCultureProvider.DefaultCookieName,
                 CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    IsEssential = true,
+                    Secure = false, // Стави true ако користиш HTTPS
+                    SameSite = SameSiteMode.Lax,
+                    Path = "/"
+                }
             );
         }
 
-        // Пренасочување назад на страницата
-        return LocalRedirect(redirectUri ?? "/");
+        // Пренасочување по поставување на културата
+        Uri redirectUriParsed;
+        if (Uri.TryCreate(redirectUri, UriKind.RelativeOrAbsolute, out redirectUriParsed))
+        {
+            if (redirectUriParsed.IsAbsoluteUri)
+            {
+                return Redirect(redirectUri); // Пренасочување ако е комплетен URL
+            }
+            else
+            {
+                return LocalRedirect(redirectUri); // Пренасочување ако е само патека
+            }
+        }
+
+        // Ако не е валиден URL, пренасочување на домашна страница
+        return LocalRedirect("/");
     }
 }
-
-
